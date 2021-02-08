6872c7189f97a5b9275b8d5b4ea0498a0b19384d
Liliana Leyva <ingeleyva@yahoo.com>
2021-02-07 18:11:48 -0600

    ..

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
-//import axios from 'axios';
+import axios from 'axios';
 import Header1 from './header'
-import picOfTheDay from './pic'
+
 
 function App() {
+    const picture =
+      "https://api.nasa.gov/planetary/apod?api_key=DkWNGZPvXKe4c374vReR3d5IgNzWnxgTJkBIYimJ";
+  
+    const [photo, setPhoto] = useState(null);
+  
+    useEffect(() => {
+      axios.get(picture)
+      .then((response) => {
+        setPhoto(response.data);
+      });
+    }, [picture]);
+
+  
+  
   return (
     <div className="App">
-      <Header1/>
-      <picOfTheDay/>
+      <h1>{photo.title}</h1>
     </div>
   );
 }
